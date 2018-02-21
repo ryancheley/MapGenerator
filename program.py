feccd7a008653a365697e8f6bc1c83fc4c0ffb42
Ryan Cheley <rcheley@gmail.com>
2018-02-20 18:26:07 -0800

    moved configuration variables out of the process_file method

--- program.py
+++ program.py
@@ -1,31 +1,31 @@
 import osmnx as ox
 from processing import get_file_data, save_images_with_name, save_images_without_name
 
+# Declare Graph Address Variables
+ADDRESS_DISTANCE = 7500
+ADDRESS_DISTANCE_TYPE = 'network'
+ADDRESS_NETWORK_TYPE = 'walk'
+
+# Declare the plot Variables
+PLOT_BACKGROUND_COLOR = '#000000'
+PLOT_NODE_SIZE = 0
+PLOT_FIGURE_HEIGHT = 40
+PLOT_FIGURE_WIDTH = 30
+PLOT_DPI = 300
+PLOT_EDGE_LINE_WIDTH = 2.0
+PLOT_FILE_TYPE = 'png'
+
+FILL = (0, 0, 0, 0)
+
 
 def main():
     ox.config(log_file=False, log_console=False, use_cache=False)
-    plot_file_type = 'png'
-
     landmarks = get_file_data('/Users/Ryan/Dropbox/Ryan/Python/landmarks single.txt')
+    process_file(landmarks, PLOT_FILE_TYPE, '#3366cc', '#cc0000')
 
-    process_file(landmarks, plot_file_type)
 
+def process_file(data, plot_file_type, short_distance_edge_color, long_distance_edge_color):
 
-def process_file(data, plot_file_type):
-    # Declare Graph Address Variables
-    address_distance = 7500
-    address_distance_type = 'network'
-    address_network_type = 'walk'
-
-    # Declare the plot Variables
-    plot_background_color = '#000000'
-    plot_node_size = 0
-    plot_figure_height = 40
-    plot_figure_width = 30
-    plot_dpi = 300
-    plot_edge_line_width = 2.0
-
-    fill = (0, 0, 0, 0)
     city = []
 
     for element in data:
@@ -35,33 +35,34 @@ def process_file(data, plot_file_type):
     for city_name in range(len(data)):
         g = ox.graph_from_address(
             data[city_name],
-            distance=address_distance,
-            distance_type=address_distance_type,
-            network_type=address_network_type,
+            distance=ADDRESS_DISTANCE,
+            distance_type=ADDRESS_DISTANCE_TYPE,
+            network_type=ADDRESS_NETWORK_TYPE,
             simplify=True,
             name=city[city_name]
         )
 
-        ec = ['#cc0000' if data['length'] >= 100 else '#3366cc' for u, v, key, data in g.edges(keys=True, data=True)]
+        ec = [long_distance_edge_color if data['length'] >= 100 else short_distance_edge_color
+              for u, v, key, data in g.edges(keys=True, data=True)]
 
         ox.plot_graph(
             g,
-            bgcolor=plot_background_color,
-            node_size=plot_node_size,
-            fig_height=plot_figure_height,
-            fig_width=plot_figure_width,
+            bgcolor=PLOT_BACKGROUND_COLOR,
+            node_size=PLOT_NODE_SIZE,
+            fig_height=PLOT_FIGURE_HEIGHT,
+            fig_width=PLOT_FIGURE_WIDTH,
             file_format=plot_file_type,
-            dpi=plot_dpi,
+            dpi=PLOT_DPI,
             filename=city[city_name],
             save=True,
             edge_color=ec,
-            edge_linewidth=plot_edge_line_width,
+            edge_linewidth=PLOT_EDGE_LINE_WIDTH,
             show=False
         )
 
         img = save_images_without_name(city[city_name], plot_file_type)
 
-        save_images_with_name(city[city_name], img, fill, plot_file_type)
+        save_images_with_name(city[city_name], img, FILL, plot_file_type)
 
 
 if __name__ == '__main__':
